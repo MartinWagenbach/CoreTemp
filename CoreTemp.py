d2a88426cb02fba42573611aa26dfa37835fd531
Martin Wagenbach <65785896+MartinWagenbach@users.noreply.github.com>
2020-06-07 22:40:37 +0200

    Implement Default Config

--- CoreTemp.py
+++ CoreTemp.py
@@ -3,6 +3,7 @@ import sys
 from configparser import ConfigParser
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from pathlib import Path
 from smtplib import SMTP
 import subprocess as sp
 import time
@@ -10,46 +11,49 @@ from os.path import expanduser
 from config_path import ConfigPath
 
 
-cfg_path = ConfigPath("", "CoreTemp", "ini")
 last_mail = None
 
 
-def createConfig():
+def readConfig():
     cp = ConfigParser()
 
-    cp["config"] = {
+    cp["default"] = {
         "log_path": "stdout",
         "update_interval": 60,
         "mail_interval": 900,
         "warning_threshold": 70,
-        "panic_threshold": 90
-    }
-    cp["mail_login"] = {
-        "smtp_server": "smtp.srv.com:587",
-        "e_mail": "user@mail.com",
-        "password": "changeme",
-        "receiver": "user@mail.com"
+        "panic_threshold": 90,
     }
 
-    print("Config file not found.")
-    print("Writing default config to {}".format(cfg_path.saveFilePath()))
-    print("Change me pls. Beep Bop.")
+    system_cfg_path = Path("/etc/CoreTemp.ini")
+    local_cfg_path = ConfigPath("", "CoreTemp", "ini")
 
-    with open(cfg_path.saveFilePath(mkdir=True), "w") as configfile:
-        cp.write(configfile)
+    if system_cfg_path.is_file():
+        cp.read(system_cfg_path)
+    if local_cfg_path.readFilePath().is_file():
+        cp.read(local_cfg_path.readFilePath())
+
+    return cp
 
 
 def sendMail(cfg, body, subject):
+    if not cfg.has_option("config", "smtp_server") or \
+            not cfg.has_option("config", "e_mail") or \
+            not cfg.has_option("config", "password") or \
+            not cfg.has_option("config", "receiver"):
+        print("E-Mails have not been configured.")
+        return
+
     message = MIMEMultipart("alternative")
     message["Subject"] = subject
-    message["From"] = cfg["mail_login"]["e_mail"]
-    message["To"] = cfg["mail_login"]["receiver"]
+    message["From"] = cfg["config"]["e_mail"]
+    message["To"] = cfg["config"]["receiver"]
 
     message.attach(MIMEText(body, "plain"))
-    server = SMTP(cfg["mail_login"]["smtp_server"])
+    server = SMTP(cfg["config"]["smtp_server"])
     server.starttls()
-    server.login(cfg["mail_login"]["e_mail"], cfg["mail_login"]["password"])
-    server.sendmail(cfg["mail_login"]["e_mail"], cfg["mail_login"]["receiver"], message.as_string())
+    server.login(cfg["config"]["e_mail"], cfg["config"]["password"])
+    server.sendmail(cfg["config"]["e_mail"], cfg["config"]["receiver"], message.as_string())
     server.quit()
 
 
@@ -76,12 +80,7 @@ def measureTemp(cfg):
         file.close()
 
 
-if not cfg_path.readFilePath():
-    createConfig()
-    sys.exit()
-
-config = ConfigParser()
-config.read(cfg_path.readFilePath())
+config = readConfig()
 
 while True:
     measureTemp(config)
